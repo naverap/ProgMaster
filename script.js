e1ad378726864381ab0cd3f1e2c05949971f3277
Noamma05 <manor.noam05@gmail.com>
2023-11-14 11:34:03 +0200

    sds

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("navToCS").addEventListener("click", function(){
+    window.location.href = "pages/CSshortCuts.html"
+});
+
+document.getElementById("navToJ").addEventListener("click", function(){
+    window.location.href = "pages/JshortCuts.html"
+});
+
+document.getElementById("navToTS").addEventListener("click", function(){
+    window.location.href = "pages/typeSpeed.html"
+});
+
+document.getElementById("register").addEventListener("click", function(){
+    window.location.href = "Register.html"
+});
